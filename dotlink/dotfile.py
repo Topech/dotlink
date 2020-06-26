5e517fdb86404f16c1ae9cb56740dc2ca3f12c29
Ben Longbottom <benjaminlongbottom98@gmail.com>
2020-06-26 23:42:50 +0800

    added operator and str representation overrides

--- dotlink/dotfile.py
+++ dotlink/dotfile.py
@@ -9,13 +9,52 @@ import pathlib
 # Class that represents a single dotfile
 ##
 class Dotfile:
-    desired_path = ""
 
-    ## set path to dotfile 
+    # TODO: name variable better
+    name = None
+    path = None
+    
+
     def __init__(self, path):
+        """
+        set path to dotfile 
+        """
 
-        self.desired_path = pathlib.Path(path)
+        self.path = pathlib.Path(path)
+        self.name = self.path.name
 
         # check if given files are valid
-        if not self.desired_path.is_file():
+        if not self.path.is_file():
             raise FileNotFoundError("the dotfile could not be found")
+
+
+
+    def __eq__(self, obj):
+        """
+        override the equals operator so dotfiles can be compared
+        """
+
+        # equality requires the dotfiles have the same name and both
+        # objects are dotfile objects
+        if isinstance(obj, Dotfile) and self.name == obj.name:
+            return True
+        else:
+            return False
+
+
+
+    def __ne__(self, obj):
+        """
+        override the not equals operator. the not of equals
+        """
+
+        return not __eq__(self, obj)
+
+
+    
+    def __str__(self):
+        """
+        override the string cnversion for dorfile
+        """
+    
+        return "Dotfile({})".format(self.name)
