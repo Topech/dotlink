ff89eea4be22826fb4170940b49c9bb0dd6336b2
NightLamp <tobrasticos@gmail.com>
2020-11-04 17:26:44 +0800

    fixed module files to remove LinkMap class references

--- dotlink/__init__.py
+++ dotlink/__init__.py
@@ -1,4 +1,3 @@
 import dotlink.app
 from dotlink.dotfile import Dotfile
 from dotlink.target import Target
-from dotlink.linkmap import LinkMap
--- dotlink/app.py
+++ dotlink/app.py
@@ -3,13 +3,9 @@
 ## 
 
 import dotlink.cli as cli
-import load_json from dotlink.linkmap
+import dotlink.linkmap
 
 
 def run():
     args = cli.process_args();
-
-    dotfiles = dict()
-    targets = dict()
-
 
